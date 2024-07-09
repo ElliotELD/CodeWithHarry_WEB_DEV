3fa03ec136f4760f552c2b75a0b088e5e870f936
ElliotELD <126496452+ElliotELD@users.noreply.github.com>
2024-07-09 21:29:08 +0530

    updated
    code updated..

---
+++ Day_11/script.js
@@ -0,0 +1,16 @@
+// console.log("hell yeah!");
+// console.log("code is live!");
+// alert("first alert");
+// alert("second alert");
+
+// console.error("my error!");
+
+// let a = prompt("enter your name!");
+// console.log(a);
+
+// let x = confirm("are you 18+");
+// console.log(x);
+
+// document.title = "go away";
+document.body.style.backgroundColor = "red";
+document.body.style.color = "orange";
